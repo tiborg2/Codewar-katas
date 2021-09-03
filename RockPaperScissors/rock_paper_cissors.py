917385f726b36730d912762f36aa956010d4ede8
tiborg2 <rytis.cesna@gmail.com>
2021-09-04 00:07:39 +0300

    rock_paper_scissors

--- RockPaperScissors/rock_paper_cissors.py
+++ RockPaperScissors/rock_paper_cissors.py
@@ -1,12 +1,20 @@
+import random as rn
+import os
+import time
 '''
 Function to play rock, paper, scissors game against AI
 '''
-import random as rn
-
 def rock_paper_scissors():
+    '''================Primary Data=========================='''
+
     choices1 = ["Rock", "Paper", "Scissors"]
-    choices = ['r', 'R', 'p', 'P', 's', 'S']
-    choice_hash = {'r':"Rock", 'p':"Paper", 's':"Scissors", 'R':"Rock", 'P':"Paper", 'S':"Scissors"}
+    choices = ['r', 'p', 's', 'q']
+    choice_hash = {'r':"Rock", 'p':"Paper", 's':"Scissors"}
+    pl_count = 0
+    ai_count = 0
+    line = f'**************************************************'
+
+    '''================Function Part========================='''
 
     def call_selector(): #Calls choice table.Utility.
         with open ('selection_text.txt', 'r') as text:
@@ -14,6 +22,9 @@ def rock_paper_scissors():
         print(sl)
         return sl
     
+    def cls(): # Terminar clearance. Utility
+        os.system('cls' if os.name=='nt' else 'clear')
+
     def ai_picker():#Randomizes AI pick
         ai_pick = rn.choice(choices)
         return ai_pick        
@@ -24,31 +35,36 @@ def rock_paper_scissors():
         else:
             return False
 
-    def pl_picker():#Player pick. 
+    def pl_picker():#Player's pick. 
         pl_pick = input('Your Choice: ')
+        pl_pick = pl_pick.lower()
         return pl_pick
     
-    '''================Action start========================='''
+    '''================Action Part=========================='''
 
     ai_pick = choice_hash.get(ai_picker())
-    print (ai_pick)
     sl = call_selector()
     pl_pick = pl_picker()
+    #print(pl_pick)
 
     #pl_pick = choice_hash.get(pl_picker())
     #print (f'"You choosed: {pl_pick}')
     
-    if pl_pick != 'q' or pl_pick != 'Q':
+    if pl_pick == 'q':
+        print ("Bye!")
+        return 0
+    else:    
         if error_handler(pl_pick):
-            print(f'Going to compare Player {pl_pick} and Computer {ai_pick}')
+            plh_pick = choice_hash.get(pl_pick)
+            cls()
+            print(line)
+            print(f'Going to compare Player: "{plh_pick}" and Computer: "{ai_pick}"')
+            print(line)
+            time.sleep(5)
         else:
             print (f'Error message')
-    else:
-        return 'O'
-
-
-
-
+    
+    return 0
 
 rock_paper_scissors()
 #print(error_handler("pl", "rocky"))
