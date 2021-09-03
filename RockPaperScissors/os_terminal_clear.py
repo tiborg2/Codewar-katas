b685326f401f2de3ace4b879d3b4148701c7adb8
tiborg2 <rytis.cesna@gmail.com>
2021-09-03 17:36:20 +0300

    RPScissors setting up

---
+++ RockPaperScissors/os_terminal_clear.py
@@ -0,0 +1,6 @@
+import os
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+cls()
--- RockPaperScissors/rock_paper_cissors.py
+++ RockPaperScissors/rock_paper_cissors.py
@@ -1,39 +1,54 @@
 '''
-Function to play rock, paper, scissors game against AI'''
+Function to play rock, paper, scissors game against AI
+'''
 import random as rn
 
 def rock_paper_scissors():
-    class AI:
-        def __init__(self, name):
-            self.name = name
-            self.score = 0
-            self.pick = ''
-        
-        def picker(self):
-            self.pick = rn.choice(["rock", "paper", "scissors"])
-            
-    computer = AI("Super Kompas")
-    computer.picker()
+    choices1 = ["Rock", "Paper", "Scissors"]
+    choices = ['r', 'R', 'p', 'P', 's', 'S']
+    choice_hash = {'r':"Rock", 'p':"Paper", 's':"Scissors", 'R':"Rock", 'P':"Paper", 'S':"Scissors"}
+
+    def call_selector(): #Calls choice table.Utility.
+        with open ('selection_text.txt', 'r') as text:
+            sl = text.read()
+        print(sl)
+        return sl
     
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-        self.pick = ''
-    
-    def error_handler(self, choice):
-        choices = ["rock", "paper", "scissors"]
-        if choice in choices:
+    def ai_picker():#Randomizes AI pick
+        ai_pick = rn.choice(choices)
+        return ai_pick        
+
+    def error_handler(pl_pick): #Handles input errors.
+        if pl_pick in choices:
             return True
         else:
             return False
+
+    def pl_picker():#Player pick. 
+        pl_pick = input('Your Choice: ')
+        return pl_pick
     
+    '''================Action start========================='''
 
-    def picker(self):
-        n = input('Please insert "r" for "Rock"')
-        pass
+    ai_pick = choice_hash.get(ai_picker())
+    print (ai_pick)
+    sl = call_selector()
+    pl_pick = pl_picker()
+
+    #pl_pick = choice_hash.get(pl_picker())
+    #print (f'"You choosed: {pl_pick}')
+    
+    if pl_pick != 'q' or pl_pick != 'Q':
+        if error_handler(pl_pick):
+            print(f'Going to compare Player {pl_pick} and Computer {ai_pick}')
+        else:
+            print (f'Error message')
+    else:
+        return 'O'
 
 
 
 
-print(error_handler("pl", "rocky"))
+
+rock_paper_scissors()
+#print(error_handler("pl", "rocky"))
