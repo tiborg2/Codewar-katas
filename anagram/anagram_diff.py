c74611417ee1472d1c660db96c5eada990cc9866
tiborg2 <rytis.cesna@gmail.com>
2021-09-11 00:23:35 +0300

    anagram differences alpha

---
+++ anagram/anagram_diff.py
@@ -0,0 +1,17 @@
+def anagram_difference(w1, w2):
+    def list_maker(w1, w2):
+        a = list(w1)
+        b = list(w2) 
+
+    a = list_maker(w1, w2)
+    b = list_maker(w1, w2)
+    
+    for i in list(a):
+        return a, b
+
+
+
+
+
+
+print (anagram_difference('solo', 'solace'))
